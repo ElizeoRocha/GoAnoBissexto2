99647c08133b55471ca19c8e0d7ce82f3e0ef602
Elizeo Rocha <48933133+ElizeoRocha@users.noreply.github.com>
2020-09-16 23:12:13 -0300

    Update AnoBissexto.go

--- AnoBissexto.go
+++ AnoBissexto.go
@@ -4,8 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"strconv"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -15,21 +20,74 @@ type Year struct {
 	ProximoAno int  `json:"proximoano"`
 }
 
-var Years []Year
+var Years []Year = []Year{}
+
+func logHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		x, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		log.Println(fmt.Sprintf("%q", x))
+		rec := httptest.NewRecorder()
+		fn(rec, r)
+		log.Println(fmt.Sprintf("%q", rec.Body))
+
+		// this copies the recorded response to the response writer
+		for k, v := range rec.HeaderMap {
+			w.Header()[k] = v
+		}
+		w.WriteHeader(rec.Code)
+		rec.Body.WriteTo(w)
+	}
+}
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "A message was received")
+}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", listarAnos).Methods("GET")
-	r.HandleFunc("/", cadastrarAnos).Methods("POST")
+
+	headers := handlers.AllowedHeaders([]string{"X-Request", "Content-Type", "Authorization"})
+	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+	origins := handlers.AllowedOrigins([]string{"http://localhost:4200"})
+
+	r.HandleFunc("/{ano}", getAno).Methods("GET")
+	r.HandleFunc("/", logHandler(listarAnos)).Methods("GET")
+	r.HandleFunc("/", logHandler(cadastrarAnos)).Methods("POST")
 	http.Handle("/", r)
 	fmt.Println("Status OK")
-	http.ListenAndServe(":8080", nil)
+	r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		t, err := route.GetPathTemplate()
+		if err != nil {
+			return err
+		}
+		fmt.Println(t)
+		return nil
+	})
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(headers, methods, origins)(r)))
+
 }
 
 func listarAnos(w http.ResponseWriter, r *http.Request) {
+	//EXIBINDO ANOS LISTADOS
 	w.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(Years)
+}
+
+func getAno(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	ano, _ := strconv.Atoi(params["ano"])
+	for _, year := range Years {
+		if year.Ano == ano {
+			json.NewEncoder(w).Encode(year)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Year{})
 }
 func cadastrarAnos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
